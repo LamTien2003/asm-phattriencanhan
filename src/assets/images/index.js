341b31370cd378791f8d15d40055a4bb43338852
duyyne <nguyenduongminhduy29122002@gmail.com>
2023-05-28 19:01:52 +0700

    28/5

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,11 +2,23 @@ import yellowLogo from './LOGO VANG.png';
 import blueLogo from './LOGO XANH.png';
 import backGroundBanner1 from './background banner 1.png';
 import backGroundBanner2 from './background banner 2.png';
+import imageAnimal3 from './IMAGE ANIMALS-03.png';
+import imageAnimal4 from './IMAGE ANIMALS-04.png';
+import imageAnimal5 from './IMAGE ANIMALS-05.png';
+import imageAnimal6 from './IMAGE ANIMALS-06.png';
+import imageAnimal7 from './IMAGE ANIMALS-07.png';
+import imageAnimal8 from './IMAGE ANIMALS-08.png';
 const images = {
     yellowLogo,
     blueLogo,
     backGroundBanner1,
     backGroundBanner2,
+    imageAnimal3,
+    imageAnimal4,
+    imageAnimal5,
+    imageAnimal6,
+    imageAnimal7,
+    imageAnimal8,
 };
 
 export default images;
