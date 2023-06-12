75f5fa193864f79d88662909650f2e9e0edb0f9a
LamTien2003 <ltt209514@gmail.com>
2023-06-12 18:07:45 +0700

    undo script tag for gg analytic

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
-
 import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from '@/redux/store.js';
 import { Provider } from 'react-redux';
 import GlobalStyles from '@/components/GlobalStyles';
 
-ReactGA.initialize('G-E5BBQC5L9W');
-const history = createBrowserHistory();
-history.listen((location) => {
-    ReactGA.pageview(location.pathname);
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Router history={history}>
+        <Router>
             <Provider store={store}>
                 <GlobalStyles>
                     <App />
